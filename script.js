929992220c59019cd9b29df2b05426da1fd1d164
parknich081 <45834520+parknich081@users.noreply.github.com>
2022-11-01 09:33:26 -0400

    e

--- script.js
+++ script.js
@@ -38,20 +38,79 @@ document.addEventListener('keydown', (e) => {
         ) + 'px';
         paddle_1_coord = paddle_1.getBoundingClientRect();
     }
-    if (e.key = 's')
+    if (e.key = 's') {
     paddle_1.style.top = 
-        Math.max(
+        Math.min(
             board_coord.bottom - paddle_common.height,
             paddle_1_coord.top + window.innerHeight * 0.06
         ) + 'px';
         paddle_1_coord = paddle_1.getBoundingClientRect();
-        )
+    }
+    if (e.key = 'arrow_up') {
+        paddle_2.style.top =
+        Math.max(
+          board_coord.top,
+          paddle_2_coord.top - window.innerHeight * 0.1
+        ) + 'px';
+        paddle_2_coord = paddle_2.getBoundingClientRect();
+    }
+    if (e.key = 'arrow_down') {
+        paddle_2.style.top =
+        Math.min(
+            board_coord.bottom - paddle_common.height,
+            paddle_2_coord.top + window.innerHeight * 0.01
+        ) + 'px';
+        paddle_2_coord = paddle_2.getBoundingClientRect();
+    }
 });
+
 
 
 function moveBall(dx, dy, dxd, dyd) {
     if (ball_coord.top <= board_coord.top) {
         dyd = 1;
     }
-    
+    if (ball_coord.bottom >= board_coord.bottom) {
+        dyd = 0;
+    }
+    if (
+        ball_coord.left <= paddle_1_coord.right &&
+        ball_coord.top >= paddle_1_coord.top &&
+        ball_coord.bottom <= paddle_1_coord.bottom
+    ) {
+        dxd = 1
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+    }
+    if (
+        ball_coord.right >= paddle_2_coord.left &&
+        ball_coord.top >= paddle_2_coord.top &&
+        ball_coord.bottom <= paddle_2_coord.bottom
+      ) {
+        dxd = 0;
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+      }
+      if (
+        ball_coord.left <= board_coord.left ||
+        ball_coord.right >= board_coord.right
+      ) {
+        if (ball_coord.left <= board_coord.left) {
+            score_2.innerHTML = +score_2.innerHTML + 1;
+          } else {
+            score_1.innerHTML = +score_1.innerHTML + 1;
+          }
+          gameState = 'start';
+          ball_coord = initial_ball_coord;
+          ball.style = initial_ball.style;
+          message.innerHTML = 'press enter';
+          message.style.left = 38 + 'vw';
+          return;
+      }
+      ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px';
+      ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px';
+      ball_coord = ball.getBoundingClientRect();
+      requestAnimationFrame(() => {
+        moveBall(dx, dy, dxd, dyd);
+      })
 }
